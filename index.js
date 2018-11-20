a52a8d16074d895354d3f19db5518d04b237464b
Normie1907 <45154936+Normie1907@users.noreply.github.com>
2018-11-20 11:37:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs")
-const moment = require("moment")
 let memeDatabase = JSON.parse(fs.readFileSync('memeDatabase.json', 'utf8'))
 const prefix = "?"//sets the prefix
 var commands = [prefix+"help",prefix+"autism",prefix+"ship",prefix+"meme add (in development)"]//list of commands
